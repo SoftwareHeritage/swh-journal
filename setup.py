7977015c98c9e49695c36ec8040ca590adaa6ca0
Nicolas Dandrimont <nicolas@dandrimont.eu>
2016-08-18 15:03:41 +0200

    import template from swh-py-template (init-py-repo)

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+setup(
+    name='swh.journal',
+    description='Software Heritage Journal utilities',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DJNL/',
+    packages=[],  # packages's modules
+    scripts=[],   # scripts to package
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
