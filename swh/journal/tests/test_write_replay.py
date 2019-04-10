730f4b911942e9f7c1ad7331fb89151e61ea660f
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-04-10 13:42:13 +0200

    Make the replayer insert objects in batches.

--- swh/journal/tests/test_write_replay.py
+++ swh/journal/tests/test_write_replay.py
@@ -85,4 +85,67 @@ def test_write_replay_same_order(objects):
             attr_name
 
 
+@given(lists(object_dicts(), min_size=1))
+@settings(suppress_health_check=[HealthCheck.too_slow])
+def test_write_replay_same_order_batches(objects):
+    committed = False
+    queue = []
+
+    def send(topic, key, value):
+        key = kafka_to_key(key_to_kafka(key))
+        value = kafka_to_value(value_to_kafka(value))
+        partition = FakeKafkaPartition(topic)
+        msg = FakeKafkaMessage(key=key, value=value)
+        if queue and {partition} == set(queue[-1]):
+            # The last message is of the same object type, groupping them
+            queue[-1][partition].append(msg)
+        else:
+            queue.append({
+                FakeKafkaPartition(topic): [msg]
+            })
+
+    def poll():
+        return queue.pop(0)
+
+    def commit():
+        nonlocal committed
+        if queue == []:
+            committed = True
+
+    storage1 = Storage()
+    storage1.journal_writer = MockedDirectKafkaWriter()
+    storage1.journal_writer.send = send
+
+    for (obj_type, obj) in objects:
+        obj = obj.copy()
+        if obj_type == 'origin_visit':
+            origin_id = storage1.origin_add_one(obj.pop('origin'))
+            if 'visit' in obj:
+                del obj['visit']
+            storage1.origin_visit_add(origin_id, **obj)
+        else:
+            method = getattr(storage1, obj_type + '_add')
+            try:
+                method([obj])
+            except HashCollision:
+                pass
+
+    queue_size = sum(len(partition)
+                     for batch in queue
+                     for partition in batch.values())
+
+    storage2 = Storage()
+    replayer = MockedStorageReplayer()
+    replayer.poll = poll
+    replayer.commit = commit
+    replayer.fill(storage2, max_messages=queue_size)
+
+    assert committed
+
+    for attr_name in ('_contents', '_directories', '_revisions', '_releases',
+                      '_snapshots', '_origin_visits', '_origins'):
+        assert getattr(storage1, attr_name) == getattr(storage2, attr_name), \
+            attr_name
+
+
 # TODO: add test for hash collision
