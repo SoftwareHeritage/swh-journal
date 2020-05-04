fa9ab16c3055e692e94040427f3ecf028de5a2bf
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-05-04 15:13:49 +0200

    serializers: Make kafka_to_key implem compatible with stable version
    The current debian storage build on stable [1] fails on an unexposed parameter.
    The current debian journal build passed on stable probably because it missed a
    test on that particular method.
    
    This commit removes the need to turn off such parameter (default to True). It's
    not needed for the kafka objects because it prevents the use of dict with null
    keys. This is not the case for our current kafka objects.
    
    This also allows to be compatible with current version onward. In effect,
    fixing the failed build.
    
    [1] https://jenkins.softwareheritage.org/view/Debian%20packages/job/debian/job/packages/job/DSTO/job/gbp-buildpackage/177/console

--- swh/journal/tests/test_serializers.py
+++ swh/journal/tests/test_serializers.py
@@ -1,10 +1,12 @@
-# Copyright (C) 2017 The Software Heritage developers
+# Copyright (C) 2017-2020 The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from collections import OrderedDict
 import itertools
+
+from collections import OrderedDict
+from typing import Iterable
 
 from swh.journal import serializers
 
@@ -51,3 +53,25 @@ def test_pprint_key():
 
             if isinstance(key, bytes):
                 assert pprinted_key == key.hex()
+
+
+def test_kafka_to_key():
+    """Standard back and forth serialization with keys
+
+    """
+    # All KeyType(s)
+    keys: Iterable[serializers.KeyType] = [
+        {"a": "foo", "b": "bar", "c": "baz",},
+        {"a": b"foobarbaz",},
+        b"foo",
+    ]
+    for object_type, objects in TEST_OBJECTS.items():
+        for obj in objects:
+            key = serializers.object_key(object_type, obj)
+            keys.append(key)
+
+    for key in keys:
+        ktk = serializers.key_to_kafka(key)
+        v = serializers.kafka_to_key(ktk)
+
+        assert v == key
